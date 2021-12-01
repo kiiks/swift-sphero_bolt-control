37b1c536f3f365a92e7a3ef36a0186347c667b49
Killian DROULEZ <killian.droulez@outlook.fr>
2021-12-01 11:38:32 +0100

    Initial Commit

---
+++ sphero_control_appUITests/sphero_control_appUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  sphero_control_appUITestsLaunchTests.swift
+//  sphero_control_appUITests
+//
+//  Created by Killian DROULEZ on 01/12/2021.
+//
+
+import XCTest
+
+class sphero_control_appUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
